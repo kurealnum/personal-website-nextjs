5d4425c87e7cd53c1374dc897d5bd03c3019bd6a
kurealnum <oscar.gaske.cs@gmail.com>
2025-04-23 17:24:27 -0400

    fix: Fixed type errors w/ deployment

--- src/hooks/useWindowSize.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from "react";
-
-export default function useWindowSize() {
-  const [windowSize, setWindowSize] = useState<[number, number]>([]);
-  useEffect(() => {
-    setWindowSize([window.innerWidth, window.innerHeight]);
-  });
-}
